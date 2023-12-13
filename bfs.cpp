caf21359820f37b41d5435b2eee8d158b1a4ad0f
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2023-12-13 08:01:45 +0530

    bfs added

---
+++ bfs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool vis[(int)(1e5+1)];
+// graph waali 
+
+void bfs(int u)
+{
+   
+}
+
+// tree waali 
+void bfs(int child,int parent)
+{
+	
+}
